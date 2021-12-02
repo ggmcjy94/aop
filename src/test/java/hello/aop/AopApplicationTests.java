82c4e872beff53496cbb42ae72b71f35878f397e
ggmcjy <you@example.com>
2021-12-02 17:41:19 +0900

    2021-12-02 commit

--- src/test/java/hello/aop/AopApplicationTests.java
+++ src/test/java/hello/aop/AopApplicationTests.java
@@ -3,11 +3,22 @@ package hello.aop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @SpringBootTest
 class AopApplicationTests {
 
 	@Test
 	void contextLoads() {
+		List<String> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			list.add(UUID.randomUUID().toString());
+		}
+		for (String s : list) {
+			System.out.println("s = " + s);
+		}
 	}
 
 }
